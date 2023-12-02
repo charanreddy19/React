f712793f3ee5f9738b7772805ccf3e62c54ead6c
Charanreddy1 <Charan.Reddy@gep.com>
2023-12-02 14:08:09 +0530

    Initital Commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const root = ReactDOM.createRoot(document.getElementById('root'));
+React.createElement('div',{},[React.createElement('h1',{},'Testing'),React.createElement('h2',{},'Testing2')])
+root.render(div);
+console.log(div);
