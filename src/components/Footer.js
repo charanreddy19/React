7d264cff5e3fd8ae46ac1c2f59df9236f132de18
Charanreddy1 <Charan.Reddy@gep.com>
2023-12-09 22:31:34 +0530

    React Hooks

---
+++ src/components/Footer.js
@@ -0,0 +1,6 @@
+const FooterComponent = ()=> {
+    return (<div className="header-component">
+        <h1>I am Footer</h1>
+    </div>)
+}
+export default FooterComponent;
---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+const HeaderComponent = ()=> {
+    return (<div className="header-component">
+        <h1>I am Header</h1>
+    </div>)
+}
+export default HeaderComponent;
